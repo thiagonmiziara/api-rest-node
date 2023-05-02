b458b948f9f0db02e652e7aad10daf6cfedc7fb1
Thiago Miziara <thiago.miziara@luizalabs.com>
2023-05-02 17:16:32 -0300

    add nova migration

--- src/server.ts
+++ src/server.ts
@@ -19,3 +19,5 @@ app
 // comando para rodar o servidor npx tsx src/server.ts
 // script no package.json "dev" para rodar servidor yarn dev
 // comando pra criar uma migration yarn knex migrate:make nome da migration
+// comando pra rodar a migration yarn knex migrate:latest
+// comando pra desfazer a migration ou alterar algum nome de campo errado antes de subir para produção yarn knex migrate:rollback
