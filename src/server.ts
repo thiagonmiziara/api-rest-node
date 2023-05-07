5fc2b8192062577e41fa74643ef5e4a758a2d88e
Thiago Miziara <thiago.miziara@luizalabs.com>
2023-05-07 13:26:55 -0300

    adicionando zod para validar .env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 // import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -20,7 +21,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')
