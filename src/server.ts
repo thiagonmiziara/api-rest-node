b70473391f8d7b6ee491e929b832c6b04790c593
Thiago Miziara <thiago.miziara@luizalabs.com>
2023-05-07 13:11:52 -0300

    adicionando variaveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  // inserindo dados na tabela transactions
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Salário',
+  //     amount: 5000,
+  //   })
+  //   .returning('*')
+
+  const transaction = await knex('transactions').select('*')
+  return transaction
 })
 
 app
