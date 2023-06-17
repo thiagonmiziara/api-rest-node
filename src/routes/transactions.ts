08c5df16d87fcec984b70fdedf082402804c0137
thiagonmiziara <thiagonmizara@gmail.com>
2023-06-17 14:56:02 -0300

    feat: add route post para criar uma transacao

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,28 @@
 import { FastifyInstance } from 'fastify'
-import knex from 'knex'
+import { z } from 'zod'
+import crypto from 'node:crypto'
+import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions').select('*')
-    return transaction
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      type,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    // status code 201 criado com sucesso
+    return reply.status(201).send()
   })
 }
